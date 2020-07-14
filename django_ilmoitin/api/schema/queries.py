9505a086595370635c88f2d914420358ddc9828a
Alex Ramirez <armzprz@gmail.com>
2020-07-14 13:15:35 +0300

    Add explicit resolver for graphql query

--- django_ilmoitin/api/schema/queries.py
+++ django_ilmoitin/api/schema/queries.py
@@ -1,7 +1,13 @@
 from graphene_django import DjangoConnectionField
 
+from django_ilmoitin.models import NotificationTemplate
+
 from .types import NotificationTemplateNode
 
 
 class Query:
     notification_templates = DjangoConnectionField(NotificationTemplateNode)
+
+    @staticmethod
+    def resolve_notification_templates(parent, info, **kwargs):
+        return NotificationTemplate.objects.all()
