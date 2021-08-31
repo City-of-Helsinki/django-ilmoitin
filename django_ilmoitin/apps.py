66eb7f02621013143a11de4a6b046e6eef43c5ca
Frank Wickström <frank.wickstrom@anders.fi>
2021-08-31 14:04:40 +0300

    Set the default auto field to BigAutoField
    This sets things up the same way as Django would do if the application were
    created with Django 3.2 and would work without breaking on Django 4.0

--- django_ilmoitin/apps.py
+++ django_ilmoitin/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class DjangoIlmoitinConfig(AppConfig):
     name = "django_ilmoitin"
     verbose_name = _("Ilmoitin")
+    default_auto_field = 'django.db.models.BigAutoField'
