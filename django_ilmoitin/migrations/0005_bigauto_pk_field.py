017921c0a5255a129a2e86e32de5849dffcb36c8
Matti Eiden <matti.eiden@anders.com>
2024-10-17 18:34:29 +0300

    style: apply black

--- django_ilmoitin/migrations/0005_bigauto_pk_field.py
+++ django_ilmoitin/migrations/0005_bigauto_pk_field.py
@@ -6,13 +6,15 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_ilmoitin', '0004_update_translation_master_type'),
+        ("django_ilmoitin", "0004_update_translation_master_type"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='notificationtemplate',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="notificationtemplate",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
